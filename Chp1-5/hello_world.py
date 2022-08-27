4765886fced5921ea088a1a52155194609092be0
ColorlessHatSpy <112276833+ColorlessHatSpy@users.noreply.github.com>
2022-08-27 16:40:27 -0400

    on second chp

---
+++ Chp1-5/hello_world.py
@@ -0,0 +1,10 @@
+message = "Hello Py World"
+name = "andrew wiley"
+first_name = "Andrew"
+last_name = "Wiley"
+full_name = f"{first_name} {last_name}"
+print(message)
+print(name.title())
+print(name.upper())
+print(name.lower())
+print(full_name)
---
+++ Chp1-5/lines.py
@@ -0,0 +1,10 @@
+# love = "Andrew\n&\nSymone"
+# print(love)
+
+# lang = "Top 10 Programming Languages:\n\n\t\t\t\t1. C\n\n\t\t\t\t2. C++\n\n\t\t\t\t3. Python\n\n\t\t\t\t4. C#\n\n\t\t\t\t5. JavaScript\n\n\t\t\t\t6. Java\n\n\t\t\t\t7. GoLang\n\n\t\t\t\t8. Rust\n\n\t\t\t\t9. PHP\n\n\t\t\t\t10. Ruby"
+# print(lang)
+
+x, y, z = 1, 2, 3
+print(x)
+print(y)
+print(z)
